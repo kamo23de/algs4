0d370023c3de73d293bd4727b41b44fa25997e9b
kamo23de <kmorisse@gmail.com>
2024-03-27 08:02:04 +0100

    StopWatch in ThreeSumFast eingebaut

--- src/main/java/edu/princeton/cs/algs4/ThreeSumFast.java
+++ src/main/java/edu/princeton/cs/algs4/ThreeSumFast.java
@@ -120,7 +120,9 @@ public class ThreeSumFast {
     public static void main(String[] args)  {
         In in = new In(args[0]);
         int[] a = in.readAllInts();
+        Stopwatch timer = new Stopwatch();
         int count = count(a);
+        StdOut.println("elapsed time = " + timer.elapsedTime());
         StdOut.println(count);
     }
 }
